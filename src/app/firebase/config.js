0bc56b826d79e7b346155eb7856c33f6bab024c3
Mahi-Eshu <maheshkumarvenkatesan03@gmail.com>
2024-09-02 20:39:11 +0530

    env

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCx_VeyD68TcMaYtarqF4yhwBsbLDCrLOs",
-  authDomain: "fund-raiser-a77f7.firebaseapp.com",
-  projectId: "fund-raiser-a77f7",
-  storageBucket: "fund-raiser-a77f7.appspot.com",
-  messagingSenderId: "167001754559",
-  appId: "1:167001754559:web:4d375202f640c81e9baf0a",
-  measurementId: "G-4WV7ZBNEJ2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
