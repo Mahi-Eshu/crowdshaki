ba6e679597e02c1aa6960c65ea00978db37b2652
Mahi-Eshu <maheshkumarvenkatesan03@gmail.com>
2024-09-02 20:56:53 +0530

    pro

--- src/app/personal_details/page.tsx
+++ src/app/personal_details/page.tsx
@@ -1,41 +1,45 @@
-"use client"
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import UserDetailsForm from '../components/UserDetailsForm';
+"use client";
+import React, { Suspense } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import UserDetailsForm from "../components/UserDetailsForm";
 import { useSearchParams } from "next/navigation";
 
 const getData = async (uid: string | null) => {
-  const res = await fetch("http://localhost:3000/api/personalDetails/fetchData", { 
+  const res = await fetch(
+    "https://crowdshaki.vercel.app/api/personalDetails/fetchData",
+    {
       method: "POST",
       headers: {
-          "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ uid })
-   });
-  if(!res.ok){
-      throw new Error("Something Went Wrong")
+      body: JSON.stringify({ uid }),
+    }
+  );
+  if (!res.ok) {
+    throw new Error("Something Went Wrong");
   }
   return res.json();
+};
 
-}
-
-const page = async() => {
+const page = async () => {
   const searchParams = useSearchParams();
   const uid = searchParams.get("userId");
-  const details = await getData(uid)
-  const user = details.user_details[0]
+  const details = await getData(uid);
+  const user = details.user_details[0];
   // console.log(uid)
 
   return (
     <div>
       <Navbar></Navbar>
-      <div className='p-4 md:flex md:flex-row md:justify-center md:items-center'>
-        <UserDetailsForm user={user}/>
+      <div className="p-4 md:flex md:flex-row md:justify-center md:items-center">
+        <Suspense fallback={<div>Loading...</div>}>
+          <UserDetailsForm user={user} />
+        </Suspense>
       </div>
       <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- src/app/raise_funds/page.tsx
+++ src/app/raise_funds/page.tsx
@@ -6,22 +6,7 @@ import Image from 'next/image'
 import RaiseFundsForm from "../components/RaiseFundsForm"
 import { useSearchParams } from 'next/navigation'
 
-// const getData = async (uid: string | null) => {
-//   const res = await fetch(
-//     "http://localhost:3000/api/raiseFunds",
-//     {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ uid }),
-//     }
-//   );
-//   if (!res.ok) {
-//     throw new Error("Something Went Wrong");
-//   }
-//   return res.json();
-// };
+
 const page = () => {
 
   const searchParams = useSearchParams();
@@ -46,4 +31,4 @@ const page = () => {
   )
 }
 
-export default page
+export default page;
