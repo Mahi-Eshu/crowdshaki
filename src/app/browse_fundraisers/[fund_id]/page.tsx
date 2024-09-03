bd1dedde0805252a9d0a7316ba6fb3d5ac0c85cb
Mahi-Eshu <maheshkumarvenkatesan03@gmail.com>
2024-09-03 16:26:48 +0530

    dev

--- src/app/browse_fundraisers/[fund_id]/page.tsx
+++ src/app/browse_fundraisers/[fund_id]/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { ObjectId } from "mongodb";
+import Image from "next/image";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
-import FundPage from "@/app/components/FundPage";
 
 const getData = async (fund: string) => {
   const res = await fetch(
@@ -18,17 +18,74 @@ const getData = async (fund: string) => {
 const page = async ({ params, searchParams }: any) => {
   const fundId = params.fund_id;
   const fundArray = await getData(fundId);
-  const fund = fundArray[0]
+  const fund = fundArray[0];
+  var neededAmount = fund.amountForFund;
+  var receivedAmount = 25000;
+  var formattedNeededAmount = neededAmount.toLocaleString();
+  var formattedReceivedAmount = receivedAmount.toLocaleString();
+  var receivedPercentage = (receivedAmount / neededAmount) * 100;
 
   return (
     <main>
-      <Navbar/>
+      <Navbar />
       <div>
         <h1 className="text-3xl font-bold text-black text-center my-10">
-            {fund.reasonForFund}
+          {fund.reasonForFund}
         </h1>
       </div>
-      <Footer/>
+      <div className="flex flex-col lg:flex-row w-full h-auto xl:h-[100vh] justify-between gap-10 my-10 lg:px-10">
+        {/* Image */}
+        <div className="w-full lg:w-2/3 h-[40vh] lg:h-2/3">
+          <Image
+            src="/assets/art2.jpg"
+            width={1000}
+            height={2000}
+            alt="Background"
+            className="w-full h-full mt-10 md:mt-0 object-scale-down"
+          />
+          {/* <div>
+            <Image
+              src="/assets/art2.jpg"
+              width={1000}
+              height={2000}
+              alt="Background"
+              className="w-full h-full mt-10 md:mt-0 object-contain"
+            />
+            <Image
+              src="/assets/art2.jpg"
+              width={1000}
+              height={2000}
+              alt="Background"
+              className="w-full h-full mt-10 md:mt-0 object-contain"
+            />
+            </div> */}
+        </div>
+        {/* Form Content */}
+        <div className="w-full lg:w-1/2 flex flex-col justify-center">
+          <button className="px-6 py-4 my-6 font-semibold text-2xl border-2 border-red-400 bg-white text-red-400 ">
+            Contribute Now
+          </button>
+          <div className="mt-2">
+            <div className="flex justify-start gap-10">
+              <span></span>
+              <span className="text-3xl font-medium">
+                Rs.{formattedReceivedAmount}/-
+              </span>
+              <span className="text-sm font-medium">Raised out of</span>
+              <span className="text-3xl font-medium">
+                Rs.{formattedNeededAmount}/-
+              </span>
+            </div>
+            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-black mt-2">
+              <div
+                className="bg-[#F74541] h-2.5 rounded-full"
+                style={{ width: `${receivedPercentage}%` }} // Adjust this percentage based on received amount/goal
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </main>
   );
 };
--- src/app/components/CrowdFundingForm.tsx
+++ src/app/components/CrowdFundingForm.tsx
@@ -1,5 +1,5 @@
+'use client';
 import React, { useState } from 'react';
-
 
 const CrowdFundingForm = () => {
     const [formValues, setFormValues] = useState({
